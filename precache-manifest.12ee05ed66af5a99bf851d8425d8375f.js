31f6de0a2405953e3ea7b070c213b21ba47ec142
Oner Elmas <oelmas@argosai.com>
2020-05-02 23:37:06 +0300

    Updates

---
+++ precache-manifest.12ee05ed66af5a99bf851d8425d8375f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b82b578c62a5c6006d0f6116ccb2e57",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "9f9994382ef5e4400ebe",
+    "url": "/monster-rolodex/static/css/main.fd12d34c.chunk.css"
+  },
+  {
+    "revision": "b8de5604a371cadca479",
+    "url": "/monster-rolodex/static/js/2.0fce7488.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.0fce7488.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f9994382ef5e4400ebe",
+    "url": "/monster-rolodex/static/js/main.f957332b.chunk.js"
+  },
+  {
+    "revision": "cb6e60652038fbdea085",
+    "url": "/monster-rolodex/static/js/runtime-main.9aac6f27.js"
+  }
+]);
